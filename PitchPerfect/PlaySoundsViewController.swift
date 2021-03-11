881fe3f54fbec0775154daa32d69ff87701e47dd
Abhijit Apte <abapte@adobe.com>
2021-03-11 12:32:09 +0530

    Rework on feedback

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -32,6 +32,13 @@ class PlaySoundsViewController: UIViewController {
 	
     override func viewDidLoad() {
         super.viewDidLoad()
+		snailButton.imageView?.contentMode = .scaleAspectFit
+		chipmunkButton.imageView?.contentMode = .scaleAspectFit
+		rabbitButton.imageView?.contentMode = .scaleAspectFit
+		vaderButton.imageView?.contentMode = .scaleAspectFit
+		echoButton.imageView?.contentMode = .scaleAspectFit
+		reverbButton.imageView?.contentMode = .scaleAspectFit
+		stopButton.imageView?.contentMode = .scaleAspectFit
 		setupAudio()
         // Do any additional setup after loading the view.
     }
--- PitchPerfect/RecordSoundViewController.swift
+++ PitchPerfect/RecordSoundViewController.swift
@@ -14,29 +14,25 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
 	@IBOutlet weak var recordingLabel: UILabel!
 	var audioRecorder: AVAudioRecorder!
 
+	struct Alerts {
+		static let DismissAlert = "Dismiss"
+		static let RecordingFailedError = "Recording Failed Error"
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 		stopRecordingButton.isEnabled = false
 	}
 
-	override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
-		print("view will appear")
+	func configureUI(isRecording: Bool) {
+		recordingLabel.text = isRecording ? "Recording in progress" : "Tap to record"
+		stopRecordingButton.isEnabled = isRecording
+		recordButton.isEnabled = !isRecording
 	}
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		print("view did appear")
-	}
-	
+
 	@IBAction func record(_ sender: Any) {
-		recordingLabel.text = "Recording in progress"
-		stopRecordingButton.isEnabled = true
-		recordButton.isEnabled = false
-//		print("record pressed")
-		
-		
+		configureUI(isRecording: true)
 		let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
 		let recordingName = "recordedVoice.wav"
 		let pathArray = [dirPath, recordingName]
@@ -54,11 +50,7 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
 	
 
 	@IBAction func stopRecord(_ sender: Any) {
-//		print("stop record pressed")
-		stopRecordingButton.isEnabled = false
-		recordButton.isEnabled = true
-		recordingLabel.text = "Tap to record"
-		
+		configureUI(isRecording: false)
 		audioRecorder.stop()
 		let audioSession = AVAudioSession.sharedInstance()
 		try! audioSession.setActive(false)
@@ -73,6 +65,7 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
 			
 		} else {
 			print("recording failed!")
+			showAlert(Alerts.RecordingFailedError, message: nil)
 		}
 	}
 
@@ -82,6 +75,12 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
 			let recordedURL = sender as! URL
 			playSoundVC.recordedAudioURL = recordedURL
 		}
+	}
+	
+	func showAlert(_ title: String, message: String?) {
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: Alerts.DismissAlert, style: .default, handler: nil))
+		self.present(alert, animated: true, completion: nil)
 	}
 }
 
